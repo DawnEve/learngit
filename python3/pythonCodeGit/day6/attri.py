cb3b88f841bd472691a3542a26e9f8c55041f8eb
Dawn <JimmyMall@live.com>
2015-09-26 21:32:21 +0800

    add day6: very hard. OOP, slots, property, multi-heri,len/str/repr/slice,RESTful,enum

---
+++ python3/pythonCodeGit/day6/attri.py
@@ -0,0 +1,21 @@
+﻿
+class Student(object):
+	name='Stu';
+	score=0;
+	def __init__(self, name):
+		self.name = name
+
+s = Student('Bob')
+s2 = Student('Jim')
+s.score = 90
+print( dir(s) );
+print('实例属性删除前：',s.score)
+del s.score
+print('实例属性删除后：',s.score)
+
+#类属性被其实例共享
+Student.num=100;
+print(s.num)
+print(s2.num)
+
+#相同名称的实例属性将屏蔽掉类属性，但是当你删除实例属性后，再使用相同的名称，访问到的将是类属性。
---
+++ python3/pythonCodeGit/day6/student_private.py
@@ -0,0 +1,33 @@
+﻿#双下划线表示的是private,私有属性和私有方法
+#但是私有的不彻底
+class Student(): 
+    def __init__(self, name, score):
+        self.name = name
+        self.__score = score #变量前加__就表示访问控制
+    def print_score(self):
+        print('%s: %s' % (self.name, self.__score))
+    def get_score(self):
+        return self.__score
+    def set_score(self, score):
+        self.__score = score
+        self.__priMethod()
+    def __priMethod(self):
+        print( '——0——This is private methond99999' )
+    def priMethod(self):
+        print( '1————This is PUBLIC methond99999' )
+
+
+bart = Student('Bart Simpson', 59)
+lisa = Student('Lisa Simpson', 87)
+
+print(bart.name)
+#print(bart.__score)
+print(bart.get_score())
+print(bart.set_score(100))
+print(bart.get_score())
+print(lisa)
+#lisa.__priMethod() 私有方法
+lisa.priMethod()
+
+#总的来说就是，Python本身没有任何机制阻止你干坏事，一切全靠自觉。
+print('正在干坏事: 直接获取私有变量',bart._Student__score)
