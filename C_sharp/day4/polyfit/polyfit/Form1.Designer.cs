aeeece15663e30bee2f4f3183bcfc19dc0e6c18b
Jimmy <JimmyMall@live.com>
2015-11-06 16:08:04 +0800

    polyfit(linear)

---
+++ C_sharp/day4/polyfit/polyfit/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace polyfit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "fit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(29, 144);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(470, 212);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(148, 10);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(351, 109);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "次数：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 423);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ C_sharp/day4/polyfit/polyfit/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace polyfit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.richTextBox2.Text = "//例如：y=a0+a1*x 返回值则为a0 a1  \r\n//例如：y=a0+a1*x+a2*x*x 返回值则为a0 a1 a2";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //数据源
+            double[] arrX = { 1, 2, 3, 4, 5.1 };
+            double[] arrY = { 5, 4, 3, 2, 1 };
+            int Pointlen=arrX.Length;
+            //拟合，返回参数列表
+            Double[] paras = CurveFit.MultiLine(arrX, arrY, Pointlen, int.Parse(this.textBox1.Text));
+
+            //输出结果
+            //y=a0+a1*x 返回值则为a0 a1
+            int len=paras.Length;
+
+            string str="";
+            for(int i=0; i<len;i++){
+                str+=paras[i].ToString() + "\r\n";
+            }
+            //显示结果
+            this.richTextBox1.Text = str;
+
+
+
+            //由参数计算回算测量值
+            double a0 = paras[0], a1 = paras[1];
+            double rss = 0, tss = 0;//残差平方和，总平方和
+
+            double[] arrX2 = new double[Pointlen];//用于浓度回算
+            double[] arrY2 = new double[Pointlen];//用于计算RSqure
+            for (int i = 0; i < Pointlen; i++) { 
+                arrX2[i]=(arrY[i]-a0)/a1;
+                arrY2[i] = a0 + a1 * arrX[i];
+
+                rss += Math.Pow(arrY[i] - arrY2[i],2);
+                tss += Math.Pow(arrY[i], 2);
+            }
+
+
+            //计算R^2
+            double RSqure = 1-rss/tss;
+
+            this.richTextBox1.Text += "\r\n"+RSqure;
+
+        }
+
+
+
+    }
+}
