3bc5663cb52f05b8e3845ea6109e50669a3bec29
dawnEve <jimmyMall@live.com>
2016-10-29 20:30:11 +0800

    Java Environment in Eclipse, and hello word

---
+++ Java/src/chapter1/HelloWorld.java
@@ -0,0 +1,11 @@
+package chapter1;
+
+public class HelloWorld {
+    /**
+    * 输出一行字符串“Hello World!”
+    * @param args
+    */
+    public static void main(String[] args) {
+    	System.out.println("Hello World11111!");
+    }
+}
